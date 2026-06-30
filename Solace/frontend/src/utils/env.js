c714243ab59e7b99d8d4ac051504b4227e38888c
Vozranow <vozranow@gmail.com>
2026-06-30 17:53:03 +0530

    Delete Solace directory

--- Solace/frontend/src/utils/env.js
+++
@@ -1,5 +0,0 @@
-export const ENV ={
-    BACKEND_URL: import.meta.env.VITE_BACKEND_URL,
-    RAZORPAY_KEY: import.meta.env.VITE_RAZORPAY_KEY,
-    RAZORPAY_SECRET: import.meta.env.VITE_RAZORPAY_SECRET,
-};
